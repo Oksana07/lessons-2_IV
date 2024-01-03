ab6c52daef0447af5981332daaef94901328b563
Oksana07 <petryshyn.v.oksana@gmail.com>
2024-01-03 18:53:15 +0200

    fix

--- js/03-lazy-load.js
+++ js/03-lazy-load.js
@@ -3,3 +3,16 @@
  * - нативна підтримка
  * - події загрузки зображень
  */
+
+const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+console.log(lazyImages);
+
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoade, { once: true });
+});
+
+function onImageLoade(evt) {
+  console.log('Картинка завантажилась');
+
+  evt.target.classList.add('appear');
+}
