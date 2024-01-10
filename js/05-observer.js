0a7b702f3795026bc55a2a1f128d4026f33d4c96
Oksana07 <petryshyn.v.oksana@gmail.com>
2024-01-10 19:33:03 +0200

    fix

--- js/05-observer.js
+++ js/05-observer.js
@@ -1,16 +1,32 @@
-const targetElement = document.querySelector('.animated-text');
+// const targetElement = document.querySelector('.animated-text');
 
-const handleObserver = entries => {
-  console.log('entries', entries);
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    } else {
-      entry.target.classList.remove('visible');
-    }
-  });
+// const handleObserver = entries => {
+//   console.log('entries', entries);
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('visible');
+//     } else {
+//       entry.target.classList.remove('visible');
+//     }
+//   });
+// };
+
+// const wotcher = new IntersectionObserver(handleObserver, { threshold: 1 });
+
+// wotcher.observe(targetElement);
+
+const videoPlayer = document.querySelector('video');
+
+const hendeleVideoObserver = () => {
+  if (!videoPlayer.paused) {
+    videoPlayer.pause();
+  } else {
+    videoPlayer.play();
+  }
 };
 
-const wotcher = new IntersectionObserver(handleObserver, { threshold: 1 });
+const watcher = new IntersectionObserver(hendeleVideoObserver, {
+  threshold: 0.5,
+});
 
-wotcher.observe(targetElement);
+watcher.observe(videoPlayer);
